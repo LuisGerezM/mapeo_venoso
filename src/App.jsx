3b3cd6c57ca4db028213574f61064c23a89dd742
LuisGerezM <luis.gerez@gmail.com>
2023-12-19 12:29:20 -0300

    ADD: styles configuring styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import Theme from './config/styles/theme';
-import MapeoVenosoView from './mapeoVenoso/presentation';
+import { useThemeApp } from './hooks/useThemeApp.hook';
+import { GlobalStyle } from './config/styles/global.styles';
+import { MapeoVenosoView } from './mapeoVenoso/presentation';
 
 function App() {
+	const { Theme } = useThemeApp();
+
 	return (
 		<Theme>
+			<GlobalStyle />
 			<MapeoVenosoView />
 		</Theme>
 	);
--- src/config/styles/responsive.js
+++
@@ -1,9 +0,0 @@
-import { screenSizes } from './screen';
-
-export const responsive = {
-	xs: `(max-width: ${screenSizes.xsMax}px)`,
-	sm: `(min-width: ${screenSizes.smMin}px) and (max-width: ${screenSizes.smMax}px)`,
-	md: `(min-width: ${screenSizes.mdMin}px) and (max-width: ${screenSizes.mdMax}px)`,
-	lg: `(min-width: ${screenSizes.lgMin}px) and (max-width: ${screenSizes.lgMax}px)`,
-	xl: `(min-width: ${screenSizes.xlMin}px)`,
-};
